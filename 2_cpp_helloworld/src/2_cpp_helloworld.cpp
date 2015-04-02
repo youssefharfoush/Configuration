bfbd07814fc452eccd5233483fe677e2c87666b5
Youssef Harfoush <youssef.harfoush1@gmail.com>
2015-04-02 19:49:06 +0300

    ADS Definitions Correct

---
+++ 2_cpp_helloworld/src/2_cpp_helloworld.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : iotdk-cpp-template.cpp
+// Author      : Intel Corporation
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Definitions.h"
+#include "ADS1299.h"
+#include "ADS1299Manager.h"
+using namespace std;
+
+int main() {
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+
+}
